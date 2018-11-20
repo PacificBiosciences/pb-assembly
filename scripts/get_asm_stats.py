83972b78be19717c1f1438340928a758bb533dd2
Gregory T. Concepcion <gconcepcion@pacificbiosciences.com>
2018-11-20 15:27:01 -0800

    add NG50 stats to script

--- scripts/get_asm_stats.py
+++ scripts/get_asm_stats.py
@@ -11,17 +11,20 @@ import argparse
 from pbcore.io import FastaReader
 
 
-def get_fasta_stats(fasta):
+def get_fasta_stats(fasta, genome_size):
     """Calculate basic fasta stats"""
     lengths = [len(record.sequence) for record in FastaReader(fasta)]
     lengths.sort(reverse=True)
     asm_contigs = len(lengths)
     asm_total_bp = sum(lengths)
 
-    def get_nstat(lens, stat):
+    def get_nstat(lens, stat, genome_size=None):
         """Calculate all N* stats"""
         lens.sort(reverse=True)
-        total = sum(lens)
+        if genome_size is not None:
+            total = genome_size
+        else:
+            total = sum(lens)
         limit = total * stat
         for num in lens:
             total -= num
@@ -31,12 +34,14 @@ def get_fasta_stats(fasta):
     asm_n50 = get_nstat(lengths, 0.50)
     asm_n90 = get_nstat(lengths, 0.10)
     asm_n95 = get_nstat(lengths, 0.05)
+    
     asm_min = lengths[-1]
     asm_max = lengths[0]
     asm_mean = asm_total_bp / asm_contigs
     asm_median = int((lengths[int(math.floor(asm_contigs * .5))] +
                       lengths[int(math.floor(asm_contigs * .5))]) / 2)
     asm_esize = sum([x*x for x in lengths]) / asm_total_bp
+
 
     fasta_stats = {'asm_contigs': asm_contigs,
                    'asm_total_bp': asm_total_bp,
@@ -48,6 +53,17 @@ def get_fasta_stats(fasta):
                    'asm_n50': asm_n50,
                    'asm_n90': asm_n90,
                    'asm_n95': asm_n95}
+
+    if genome_size is not None:
+        asm_ng50 = get_nstat(lengths, 0.50, genome_size)
+        asm_ng90 = get_nstat(lengths, 0.10, genome_size)
+        asm_ng95 = get_nstat(lengths, 0.05, genome_size)
+    
+
+        fasta_stats.update({'asm_ng50': asm_ng50,
+                            'asm_ng90': asm_ng90,
+                            'asm_ng95': asm_ng95})
+
 
     return fasta_stats
 
@@ -65,8 +81,12 @@ def main():
             fasta = args.asm_dir
         else:
             print "Please specify a fasta or a 2-asm-falcon dir!"
+    if args.genome_size is not None:
+        genome_size = args.genome_size
+    else:
+        genome_size = None
 
-    fasta_stats = get_fasta_stats(fasta)
+    fasta_stats = get_fasta_stats(fasta, genome_size)
 
     if args.output:
         if os.path.isdir(args.output):
@@ -90,6 +110,8 @@ def get_parser():
                         help="path to 2-asm-falcon directory or fasta file")
     parser.add_argument('--output', type=str,
                         help="path to outdir or filename")
+    parser.add_argument('--genome_size', type=int,
+                        help="if known; genome_size for NG50 calculation")
 
     return parser
 
