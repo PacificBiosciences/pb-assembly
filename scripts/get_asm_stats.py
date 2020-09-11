3971e9b07b9029c5d190622222423e1c85859e13
Greg <gconcepcion@pacificbiosciences.com>
2020-09-10 20:41:50 -0700

    finally push py3 update

--- scripts/get_asm_stats.py
+++ scripts/get_asm_stats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 @gconcepcion
 Get FALCON assembly stats in JSON format
@@ -8,12 +8,26 @@ import sys
 import json
 import math
 import argparse
-from pbcore.io import FastaReader
+from itertools import groupby
 
+def fasta_iter(fasta_name):
+    """
+    https://www.biostars.org/p/710/#120760    
+    """
+    fasta = open(fasta_name)
+    faiter = (x[1] for x in groupby(fasta, lambda line: line[0] == ">"))
+
+    for header in faiter:
+        header = header.__next__()[1:].strip()
+        seq = "".join(s.strip() for s in faiter.__next__())
+        yield (header, seq)
 
 def get_fasta_stats(fasta, genome_size):
     """Calculate basic fasta stats"""
-    lengths = [len(record.sequence) for record in FastaReader(fasta)]
+
+    fiter = fasta_iter(fasta)
+
+    lengths = [len(record[1]) for record in fasta_iter(fasta)]
     lengths.sort(reverse=True)
     asm_contigs = len(lengths)
     asm_total_bp = sum(lengths)
@@ -34,13 +48,12 @@ def get_fasta_stats(fasta, genome_size):
     asm_n50 = get_nstat(lengths, 0.50)
     asm_n90 = get_nstat(lengths, 0.10)
     asm_n95 = get_nstat(lengths, 0.05)
-    
     asm_min = lengths[-1]
     asm_max = lengths[0]
-    asm_mean = asm_total_bp / asm_contigs
+    asm_mean = int(asm_total_bp / asm_contigs)
     asm_median = int((lengths[int(math.floor(asm_contigs * .5))] +
                       lengths[int(math.floor(asm_contigs * .5))]) / 2)
-    asm_esize = sum([x*x for x in lengths]) / asm_total_bp
+    asm_esize = int(sum([x*x for x in lengths]) / asm_total_bp)
 
 
     fasta_stats = {'asm_contigs': asm_contigs,
@@ -58,7 +71,6 @@ def get_fasta_stats(fasta, genome_size):
         asm_ng50 = get_nstat(lengths, 0.50, genome_size)
         asm_ng90 = get_nstat(lengths, 0.10, genome_size)
         asm_ng95 = get_nstat(lengths, 0.05, genome_size)
-    
 
         fasta_stats.update({'asm_ng50': asm_ng50,
                             'asm_ng90': asm_ng90,
@@ -80,7 +92,7 @@ def main():
         if args.asm_dir.endswith(('.fa', '.fasta')):
             fasta = args.asm_dir
         else:
-            print "Please specify a fasta or a 2-asm-falcon dir!"
+            print("Please specify a fasta or a 2-asm-falcon dir!")
     if args.genome_size is not None:
         genome_size = args.genome_size
     else:
@@ -97,7 +109,7 @@ def main():
 
             json.dump(fasta_stats, outfile, indent=1, sort_keys=True)
 
-    print json.dumps(fasta_stats, indent=1, sort_keys=True)
+    print(json.dumps(fasta_stats, indent=1, sort_keys=True))
 
 
 def get_parser():
